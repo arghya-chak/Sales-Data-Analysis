bc83eee8900664c540b79e4d0eb1803bbd85c856
arghya-chak <78783729+arghya-chak@users.noreply.github.com>
2024-07-06 12:23:35 +0530

    Update SalesDataAnalysisSQL.sql

--- SalesDataAnalysisSQL.sql
+++ SalesDataAnalysisSQL.sql
@@ -33,8 +33,8 @@ WHERE date.year = 2020;
 # Show total revenue in INR in year 2019
 SELECT SUM(t.sales_amount_INR) 
 FROM (SELECT transactions.order_date, 
-			 IF(transactions.currency = "INR", transactions.sales_amount, transactions.sales_amount*83.5) AS sales_amount_INR
-	  FROM transactions) AS t
+	     IF(transactions.currency = "INR", transactions.sales_amount, transactions.sales_amount*83.5) AS sales_amount_INR
+      FROM transactions) AS t
 INNER JOIN date 
 ON t.order_date = date.date 
 WHERE date.year = 2019;
@@ -43,7 +43,7 @@ WHERE date.year = 2019;
 SELECT SUM(t.sales_amount_INR) 
 FROM (SELECT transactions.order_date, 
              IF(transactions.currency = "INR", transactions.sales_amount, transactions.sales_amount*83.5) AS sales_amount_INR
-	  FROM transactions) AS t
+      FROM transactions) AS t
 INNER JOIN date 
 ON t.order_date = date.date 
 WHERE date.year = 2018
@@ -52,8 +52,8 @@ AND date.month_name = "May";
 # Show total revenue in INR in year 2019 in Chennai
 SELECT SUM(t.sales_amount_INR) 
 FROM (SELECT transactions.order_date, transactions.market_code, 
-			 IF(transactions.currency = "INR", transactions.sales_amount, transactions.sales_amount*83.5) AS sales_amount_INR
-	  FROM transactions) AS t
+	     IF(transactions.currency = "INR", transactions.sales_amount, transactions.sales_amount*83.5) AS sales_amount_INR
+      FROM transactions) AS t
 INNER JOIN date 
 ON t.order_date = date.date 
 WHERE date.year = 2019
